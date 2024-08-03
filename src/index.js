f6174cddcfd387f91510bdc1ad7c2c3c33085976
MaximVolohov <maxim_volohov@mail.ru>
2024-08-03 12:37:20 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+	let resultReverse = Math.abs(n).toString();
+	let result = '';
+	for (let i = resultReverse.length-1; i >= 0; i--) {
+		result += resultReverse[i]
+	}
+	return +result;
 }
